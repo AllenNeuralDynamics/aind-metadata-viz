2af36a3e9092d21dfae58759e27214d7d2926017
Dan Birman <danbirman@gmail.com>
2025-04-21 11:16:04 -0700

    feat: functional query builder

--- src/aind_metadata_viz/query.py
+++ src/aind_metadata_viz/query.py
@@ -1,6 +1,7 @@
 """App for generating metadata queries"""
 import os
 
+import pandas as pd
 import panel as pn
 import param
 
@@ -87,7 +88,7 @@ class QueryPanel(param.Parameterized):
         query_dict = {}
 
         if self.project_name != "":
-            query_dict["project_name"] = self.project_name
+            query_dict["data_description.project_name"] = self.project_name
 
         self.query_pane.value = query_dict
 
@@ -105,7 +106,64 @@ class QueryPanel(param.Parameterized):
         )
 
 
+class QueryResult(param.Parameterized):
+    """Class for displaying query results"""
+
+    query = param.Dict(default={})
+
+    def __init__(self, **params):
+        super().__init__(**params)
+
+        self.query = {}
+        self.query_pane = pn.pane.DataFrame(
+            escape=False,
+            index=False,
+        )
+
+    def update_query(self, query: dict):
+        """Update the query and fetch results"""
+        self.query = query
+
+        if self.query:
+            self.query_result = docdb_api_client.retrieve_docdb_records(
+                filter_query=self.query,
+                projection={
+                    "name": 1,
+                }
+            )
+
+            df = pd.DataFrame(self.query_result)
+            # Add a column that generates a link to the view app
+            df["View Record"] = [
+                f'<a href="http://localhost:5006/view?name={record["name"]}" target="_blank">View</a>'
+                for record in self.query_result
+            ]
+
+            self.query_pane.object = df
+
+    def panel(self):
+        """Return the query result panel"""
+        return pn.Column(
+            self.query_pane,
+            width=FIXED_WIDTH-50,
+            styles=outer_style,
+        )
+
+
 query_panel = QueryPanel()
+query_result = QueryResult()
+
+
+# Link the query_panel parameters to the query_result update_query function
+def sync_query_result(*events):
+    query_result.update_query(query_panel.query_pane.value)
+
+
+# Watch for changes in the query_panel parameters
+query_panel.param.watch(sync_query_result, ['project_name'])
+
+
+# SET UP LAYOUT
 
 header = pn.pane.Markdown(
     """
@@ -118,6 +176,7 @@ main_col = pn.Column(
     header,
     query_panel.options_panel(),
     query_panel.query_panel(),
+    query_result.panel(),
     styles=outer_style,
     width=FIXED_WIDTH,
 )
