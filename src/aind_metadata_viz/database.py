b250180dbcde121a224ba3f2c5a85ecbccf32524
Dan Birman <danbirman@gmail.com>
2025-12-04 15:29:17 -0800

    fix: back to readonly

--- src/aind_metadata_viz/database.py
+++ src/aind_metadata_viz/database.py
@@ -20,7 +20,7 @@ from aind_metadata_validator.mappings import (
 from aind_metadata_viz.utils import METASTATE_MAP, hd_style
 
 DEV_OR_PROD = "prod"
-REDSHIFT_SECRETS = f"/aind/{DEV_OR_PROD}/redshift/credentials/readwrite"
+REDSHIFT_SECRETS = f"/aind/{DEV_OR_PROD}/redshift/credentials/readonly"
 RDS_TABLE_NAME = f"metadata_status_{DEV_OR_PROD}_v2"
 
 CHUNK_SIZE = 1000
